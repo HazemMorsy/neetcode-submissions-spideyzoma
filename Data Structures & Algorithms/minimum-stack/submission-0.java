2be58598ee80cc25346286305e796ca3c3424217
HazemMorsy <34711252+HazemMorsy@users.noreply.github.com>
2026-04-09 16:41:38 +0200

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,35 @@
+class MinStack {
+
+
+    ArrayList<Integer> stack;
+    ArrayList<Integer> trackMin;
+    int min = 0;
+
+    public MinStack() {
+        stack = new ArrayList<>();
+        trackMin = new ArrayList<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+        if (trackMin.isEmpty()) {
+            trackMin.add(val);
+        } else {
+            trackMin.add(Math.min(val, trackMin.get(trackMin.size()-1)));
+        }
+    }
+    
+    public void pop() {
+        stack.remove(stack.size()-1);
+        trackMin.remove(trackMin.size()-1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size()-1);
+    }
+    
+    public int getMin() {
+        return trackMin.get(trackMin.size()-1);
+    }
+}
